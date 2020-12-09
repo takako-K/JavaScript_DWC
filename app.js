38900998990b10a346807d4eea4431620a7006fd
takako-K <takako.k828@gmail.com>
2020-12-09 20:20:27 +0900

    [Start]JavaScript②

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-alert('Hello World');
+var hello = 'Hello World';
+alert(hello);
+
+var int1 = 1;
+alert(int1);
+
+var int2 = -10;
+alert(int2);
+
+var float1 =3.14;
+alert(float1);
+
+var str1 = 'Javascriptを覚えよう';
+alert(str1);
+
+alert(4 + 3);
+
+alert(8 - 5);
+
+alert(2 * 6);
+
+alert(10 / 2);
+
+alert('Hello' + 'World');
+
+var str1 = 'Hello';
+var str2 = 'World!!';
+alert(str1 + str2);
